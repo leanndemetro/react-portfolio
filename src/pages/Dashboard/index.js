3041a0c38404a35d05a06271163365db7a083f58
Alyssa DeMetro <leanndemetro@gmail.com>
2021-02-18 13:53:57 -0500

    dashboard css update and links hooked up

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -8,9 +8,9 @@ function Dashboard() {
       <img className="textWrap" src="https://i.imgur.com/oJ2NHaR.jpg"></img>
       <p className="alyssaD">Alyssa DeMetro</p>
       <p className="navLinks"> 
-      ABOUT / 
-      PORTFOLIO /
-      CONTACT
+      <a className="linksOne" href="/About">ABOUT</a> / 
+      <a className="linksTwo" href="/Portfolio"> PORTFOLIO</a> /
+      <a className="linksThree" href="/Contact"> CONTACT</a>
       </p>
    </div>
    
