b9f33e493307622aa745625c608e1566ad168ec2
Alyssa DeMetro <leanndemetro@gmail.com>
2021-02-18 14:18:25 -0500

    css changes to dashboard page, need to edit text-wrap img to fix into rows and cols

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -4,9 +4,11 @@ import "./style.css"
 
 function Dashboard() {
   return (
-    <div>
-      <img className="textWrap" src="https://i.imgur.com/oJ2NHaR.jpg"></img>
-      <p className="alyssaD">Alyssa DeMetro</p>
+    <div className="dashCard">
+      <img className="textWrap" src="https://i.imgur.com/wZQSWEt.jpg"></img>
+      <p className="alyssaD">Alyssa DeMetro
+      <br></br>
+      </p>
       <p className="navLinks"> 
       <a className="linksOne" href="/About">ABOUT</a> / 
       <a className="linksTwo" href="/Portfolio"> PORTFOLIO</a> /
