d729e772688fa9dfcfc0ef5712df4bd4b0d8ed18
Alyssa DeMetro <leanndemetro@gmail.com>
2021-02-22 09:23:08 -0500

    theme change back to blue

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -9,8 +9,8 @@ function Dashboard() {
       <br></br> DeMetro
       </p>
       <p className="navLinks"> 
-      <a className="linksOne" href="/About">ABOUT</a> / 
-      <a className="linksTwo" href="/Portfolio"> PORTFOLIO</a> /
+      <a className="linksOne" href="/About">ABOUT /</a> 
+      <a className="linksTwo" href="/Portfolio"> PORTFOLIO /</a>
       <a className="linksThree" href="/Contact"> CONTACT</a>
       </p>
    </div>
