64c9d5ffb5bcda1f6b100cbc72b256e1221eb27d
Alyssa DeMetro <leanndemetro@gmail.com>
2021-02-24 12:25:09 -0500

    portfolio page roughly complete

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -1,18 +1,50 @@
 import React from "react";
 import "./style.css"
 import { Container, Row, Col } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 
 
 function Portfolio() {
   return (
     <Container>
     <Row>
-      <Col>
+    <Col sm={2}>
+       </Col>
+      <Col sm={8}>
    <span className="TITLE">
     Portfolio.
      </span>
-     </Col>
+    </Col>
+    <Col sm={2}>
+       </Col>
    </Row>
+   <Row>
+     <Col>
+     <hr></hr></Col>
+   </Row>
+   <Row>
+     <Col sm={6}>
+       <div className="portCardLeft">
+         <a target="_blank" className="certImg" href="https://murmuring-meadow-11462.herokuapp.com/"><img alt="budgetTracker" className="portImg" src="https://i.imgur.com/YxrUmX9.jpg"></img></a> </div>
+     </Col>
+     <Col sm={6}>
+     <div className="portCardLeft">
+     <a target="_blank" className="certImg" href="https://safe-brook-64265.herokuapp.com/"><img alt="workoutTracker" className="portImgTwo" src="https://i.imgur.com/py6Xoo5.jpg"></img></a>
+      </div>
+       </Col>
+   </Row>
+   <Row>
+     <Col sm={6}>
+       <div className="portCardLeft">
+       <a target="_blank" className="certImg" href="https://shrouded-stream-06971.herokuapp.com/"><img alt="eatDaBurger" className="portImgThree" src="https://i.imgur.com/m3SnOYK.jpg"></img></a> </div>
+     </Col>
+     <Col sm={6}>
+       </Col>
+   </Row>
+   <Row>
+   <br></br></Row>
+   <hr></hr>
+   <br></br>
    </Container>
 
   );
